8f8fa152bf38d6ceb368aba19e4e3c519469e2a6
Farhad <>
2013-08-17 18:12:28 +0430

    Add compareDate method. It is bugy yet.

--- gCal.php
+++ gCal.php
@@ -41,6 +41,55 @@ class gCal {
         return $destinationDate;
     }
 
+    /**
+     * Compare two date with each other.
+     * @param type $date1 First date for compare.
+     * @param type $format1 Format of first date.
+     * @param type $date2 Second date for compare with.
+     * @param type $format2 Format of second date.
+     * @return int If date1 is greater than date2 return 1 else if is smaller then return -1 and if equal return 0.
+     */
+    static function compareDate($date1, $format1, $date2, $format2) {
+        $date1 = date_parse_from_format($format1, $date1);
+        $date2 = date_parse_from_format($format2, $date2);
+
+        $diff_year = $date1['year'] - $date2['year'];
+        if ($diff_year > 0) return 1;
+        else if ($diff_year < 0) return -1;
+        else if ($diff_year == 0) {
+
+            $diff_month = $date1['month'] - $date2['month'];
+            if ($diff_month > 0) return 1;
+            else if ($diff_month < 0) return -1;
+            else if ($diff_month == 0) {
+
+                $diff_day = $date1['day'] - $date2['day'];
+                if ($diff_day > 0) return 1;
+                else if ($diff_day < 0) return -1;
+                else if ($diff_day == 0) {
+
+                    $diff_hour = $date1['hour'] - $date2['hour'];
+                    if ($diff_hour > 0) return 1;
+                    else if ($diff_hour < 0) return -1;
+                    else if ($diff_hour == 0) {
+
+                        $diff_minute = $date1['minute'] - $date2['minute'];
+                        if ($diff_minute > 0) return 1;
+                        else if ($diff_minute < 0) return -1;
+                        else if ($diff_minute == 0) {
+
+                            $diff_second = $date1['second'] - $date2['second'];
+                            if ($diff_second > 0) return 1;
+                            else if ($diff_second < 0) return -1;
+                            else if ($diff_second == 0) return 0;
+                            //bug in compare am and pm
+                        }
+                    }
+                }
+            }
+        }
+    }
+
 }
 
 ?>
